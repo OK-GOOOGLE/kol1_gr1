403244b60b120034a96c9e31e7334def5c90f6f6
Yevhenii Natalenko <work.natalenko@gmail.com>
2018-03-17 19:30:00 +0100

    final version with all requirements implemented

--- task.py
+++ task.py
@@ -1,47 +1,31 @@
-class Matrix:
-    def __init__(self, a, b, c, d):
-        self.dim = 2
-        self.values = ((a, b), (c, d))
 
-    def add(self, rhs):
-        newA = self.values[0][0] + rhs.values[0][0]
-        newB = self.values[0][1] + rhs.values[0][1]
-        newC = self.values[1][0] + rhs.values[1][0]
-        newD = self.values[1][1] + rhs.values[1][1]
-        return Matrix(newA, newB, newC, newD)
+#NOTE!!! python3.6 or newer is required.
+# new string format was used. (f"string: {variable}")
+# generator was used in line 24 in file Matrix.py
 
-    def multip(self, rhs):
-        if self.dim != rhs.dim:
-            print ("Matrices can not be multiplied! \n")
-            return
+from Matrix import Matrix
 
-        newA = self.values[0][0] * rhs.values[0][0] + self.values[0][1] * rhs.values[1][0]
-        newB = self.values[0][0] * rhs.values[0][0] + self.values[0][1] * rhs.values[1][1]
-        newC = self.values[1][0] * rhs.values[0][0] + self.values[1][1] * rhs.values[1][0]
-        newD = self.values[0][1] + rhs.values[0][0] + self.values[0][1] + rhs.values[1][1]
-        return Matrix(newA, newB, newC, newD)
+if __name__ == '__main__':
+    matr1 = Matrix([1, 1, 1], [1, 1, 1], [2, 2, 2])
+    print("matrix 1")
+    print (matr1)
 
-    def __str__(self):
-        returnVal = []
+    matr2 = Matrix([1, 1, 1], [1, 1, 1], [2, 2, 2])
+    print("matrix 2")
+    print (matr2)
 
-        for i in self.values:
-            temp = []
-            temp.append(i[0])
-            temp.append(i[1])
+    matr3 = matr1 + matr2
+    matr3_1 = matr1 + 1
 
-            returnVal.append(temp)
-        return str(returnVal[0][0]) + ' ' + str(returnVal[0][1]) + '\n' + str(returnVal[1][0]) + ' ' + str(returnVal[1][1]) + '\n'
-
-
-matr1 = Matrix(4, 5, 6, 7)
-print (matr1)
-matr2 = Matrix(2, 2, 2, 1)
-
-print (matr2)
-
-matr3 = matr1.add(matr2)
-print(matr3)
-
-matr4 = matr1.multip(matr2)
-
-print(matr4)
+    print("matr1 + matr2")
+    print(matr3)
+    print("matr1 + 1")
+    print(matr3_1)
+    
+    matr4 = matr1 * matr2
+    matr4_1 = matr1 * 1
+    
+    print("matr1 * matr2")
+    print(matr4)
+    print("matr1 * 1")
+    print(matr4_1)
